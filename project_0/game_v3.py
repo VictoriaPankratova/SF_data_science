36c77db8ffda94c9c0d1c224b5962e8f756cf31a
VictoriaPankratova <victoria.pankrato@gmail.com>
2023-10-12 18:33:32 +0200

    Upd game version

---
+++ project_0/game_v3.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def score_game(game_core_v3) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(game_core_v3(number))
+
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    low = 1
+    high = 100
+    predict_number = np.random.randint(1, 101) # предполагаемое число
+
+    while predict_number != number:
+        count += 1
+        predict_number = (low + high)//2
+        if predict_number > number:
+            high = predict_number
+        elif predict_number < number:
+            low = predict_number + 1      
+
+    return count
+
+print('Run benchmarking for game_core_v3: ', end='')
+score_game(game_core_v3)
